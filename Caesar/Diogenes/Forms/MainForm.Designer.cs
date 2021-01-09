9dfd7b20c26bdace4de64d684884bd8982e0fa50
N0cynym <75754060+N0cynym@users.noreply.github.com>
2021-01-09 14:59:39 +0100

    Drag and Drop
    How about loading files by drag and drop?
    Sorry I'm not a professional in C# so I'm still trying, failing and sending an idea...

--- Caesar/Diogenes/Forms/MainForm.Designer.cs
+++ Caesar/Diogenes/Forms/MainForm.Designer.cs
@@ -299,6 +299,9 @@
             // 
             // tvMain
             // 
+            this.tvMain.AllowDrop = true;
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.loadCBFFiles_DragEnter);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.loadCBFFiles_DragDrop);
             this.tvMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
