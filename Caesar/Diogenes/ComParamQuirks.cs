9123efb78fbda0389b3f3fef3e9a049d6b39bd0c
JinGen Lim <1116555+jglim@users.noreply.github.com>
2025-03-13 21:37:37 +0800

    KI211 quirk: fix bad TP scaling
    CP.TesterPresentTime / CP.R1_SUG has a ConversionFactor of 0.001M
    Either use CP.TesterPresentTime = 1, or  CP.R1_SUG = 1000

--- Caesar/Diogenes/ComParamQuirks.cs
+++ Caesar/Diogenes/ComParamQuirks.cs
@@ -19,7 +19,7 @@ namespace Diogenes
                 {
                     quirks.Add(CP.NONSTANDARD_FUNCTIONAL_INIT_TXCOUNT, 5);
                     quirks.Add(CP.TESTERPRESENT_MESSAGE, 0x3E02);
-                    quirks.Add(CP.TesterPresentTime, 1000);
+                    quirks.Add(CP.TesterPresentTime, 1);
                 }
             }
 
