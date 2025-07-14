e03f8bcd6f702758a787a401c5e06bcece60956c
Mattwmaster58 <mattwmaster58@gmail.com>
2025-07-14 07:33:10 -0700

    ft: command line file handler

--- Caesar/Diogenes/Forms/MainForm.cs
+++ Caesar/Diogenes/Forms/MainForm.cs
@@ -51,7 +51,7 @@ namespace Diogenes.Forms
         FlashView FlashViewControl = null;
         VarcodingView VarcodingViewControl = null;
 
-        public MainForm()
+        public MainForm(string commandLinePath)
         {
             InitializeComponent();
 
@@ -86,7 +86,12 @@ namespace Diogenes.Forms
             VarcodingViewControl.Dock = DockStyle.Fill;
             tabVarCoding.Controls.Add(VarcodingViewControl);
 
-            this.Text = $"Diogenes II (Build: {LinkerTime.GetLinkerTime().ToShortDateString()})";
+            Text = $"Diogenes II (Build: {LinkerTime.GetLinkerTime().ToShortDateString()})";
+
+            if (!String.IsNullOrEmpty(commandLinePath))
+            {
+                LoadCBF(commandLinePath);
+            }
         }
 
         private void MainForm_Load(object sender, EventArgs e)
